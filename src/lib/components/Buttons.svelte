d03c5afbecc16d512c1edea5ccab4df76b04c9ea
balazsmanus <contact@mnus.moe>
2025-05-14 12:33:48 +0200

    add basic button, will continue later

---
+++ src/lib/components/Buttons.svelte
@@ -0,0 +1,12 @@
+<script>
+	export let icon = '';
+	export let label = 'link';
+	export let href = '/';
+</script>
+
+<a {href} {...$$restProps}>
+	<slot>
+		<img src={icon} alt="" />
+		<p>{label}</p>
+	</slot>
+</a>
