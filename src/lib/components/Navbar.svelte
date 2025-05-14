6848384d8181580cc0ab73303511dd1c45385e3e
balazsmanus <contact@mnus.moe>
2025-05-14 12:30:11 +0200

    almost done css

---
+++ src/lib/components/Navbar.svelte
@@ -0,0 +1,49 @@
+<script lang="ts">
+    import '$lib/global.css';
+
+    const navLinks = [
+        { name: 'Github', url: 'https://github.com/QwIT-Development', style: 'button_small_secondary'},
+        { name: 'Letöltés', url: '/downloads', style: 'button_small_primary'}
+    ]
+</script>
+
+    <div class="branding">
+        <img src="/dave.svg" alt="logo" />
+        <h1>Firka Napló</h1>
+    </div>
+    <div class="links">
+
+    </div>
+
+<style>
+    div.branding {
+        display: inline-flex;
+        align-items: center;
+        gap: 16px;
+        top: 48px;
+        left: 48px;
+        z-index: 1000;
+        position: fixed;
+        height: 40px;
+    }
+
+    div.links {
+        display: inline-flex;
+        align-items: center;
+        gap: 8px;
+        right: 48px;
+        top: 48px;
+        z-index: 1000;
+        position: fixed;
+        height: 40px;
+    }
+
+    div.branding img {
+        width: 40px;
+        height: 40px;
+    }
+
+    div.branding h1 {
+        color: var(--text_primary);
+    }
+</style>
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,2 +1,9 @@
-<h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://svelte.dev/docs/kit">svelte.dev/docs/kit</a> to read the documentation</p>
+<script lang="ts">
+    import '$lib/global.css';
+</script>
+
+<style>
+    :global(body) {
+        background: var(--background);
+    }
+</style>
