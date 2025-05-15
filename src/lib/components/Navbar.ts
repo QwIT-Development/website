1f8c525fcc262713a18f14944d2acd95f2a1f199
balazsmanus <contact@mnus.moe>
2025-05-15 16:47:13 +0200

    jo mostmar ez is megy

--- src/lib/components/Navbar.ts
+++ src/lib/components/Navbar.ts
@@ -1,5 +1,6 @@
 export const navLinks: { name: string; url: string; style: 'small_secondary' | 'small_primary' }[] = [
     { name: 'GitHub', url: 'https://github.com/QwIT-Development', style: 'small_secondary' },
+    { name: 'Docs', url: '/docs', style: 'small_secondary'},
     { name: 'Letöltés', url: '/downloads', style: 'small_primary' }
 ];
 
--- src/routes/docs/+page.svelte
+++ src/routes/docs/+page.svelte
@@ -1,7 +1,27 @@
 <script lang="ts">
-    import {goto} from '$app/navigation';
+    import '$lib/global.css';
+    import {browser} from '$app/environment';
     import {onMount} from 'svelte';
+    
     onMount(() => {
-        goto('https://docs.qwit.org');
-    })
+        if (browser) {
+            window.location.href = 'https://docs.qwit.org';
+        }
+    });
 </script>
+
+<h1 style="font_web_h1">Átirányítás...</h1>
+<a href="https://docs.qwit.org">Ha nem működik, kattints ide</a>
+
+<style>
+    h1 {
+        color: var(--text_primary);
+    }
+    a {
+        color: var(--text_teritary);
+        transition: color 0.3s ease;
+    }
+    a:hover {
+		color: var(--text_primary);
+	}
+</style>
