aa677d2cd88f51e7944c4fd3568c1a478fde8f65
balazsmanus <contact@mnus.moe>
2025-05-15 16:37:14 +0200

    faszom mdbe

--- svelte.config.js
+++ svelte.config.js
@@ -1,40 +1,5 @@
 import adapter from '@sveltejs/adapter-auto';
 import { vitePreprocess } from '@sveltejs/vite-plugin-svelte';
-import { mdsvex } from 'mdsvex';
-import { visit } from 'unist-util-visit';
-
-const addClass = (node, className) => {
-  if (!node.properties) {
-    node.properties = {};
-  }
-  if (!node.properties.className) {
-    node.properties.className = [];
-  }
-  if (!Array.isArray(node.properties.className)) {
-    node.properties.className = [node.properties.className];
-  }
-  node.properties.className.push(className);
-};
-
-const rehypeTags = () => {
-  const tagClasses = {
-    'p': 'font_body_16px_regular',
-    'li': 'font_body_16px_regular',
-    'h1': 'font_header_h1',
-    'h2': 'font_header_h2',
-    'h3, h4, h5, h6': 'font_header_18px'
-  };
-
-  return (tree) => {
-    Object.entries(tagClasses).forEach(([tags, className]) => {
-      tags.split(', ').forEach(tag => {
-        visit(tree, { tagName: tag.trim() }, (node) => {
-          addClass(node, className);
-        });
-      });
-    });
-  };
-};
 
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
@@ -42,10 +7,6 @@ const config = {
 	// for more information about preprocessors
 	preprocess: [
 		vitePreprocess(),
-		mdsvex({
-			extensions: ['.md'],
-			rehypePlugins: [rehypeTags]
-		})
 	],
 
 	kit: {
@@ -54,7 +15,7 @@ const config = {
 		// See https://svelte.dev/docs/kit/adapters for more information about adapters.
 		adapter: adapter()
 	},
-	extensions: ['.svelte', '.md']
+	extensions: ['.svelte']
 };
 
 export default config;
