dd11b5ae4d4a15855dce6ade080d97cafc890e97
balazsmanus <contact@mnus.moe>
2025-06-23 17:08:17 +0200

    fix website breaking

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,44 +1,62 @@
 <script lang="ts">
-    import '$lib/global.css';
-    import Navbar from '$lib/components/Navbar.svelte';
-    import NavbarShared from '$lib/components/NavbarShared.svelte';
-    import Footer from '$lib/components/Footer.svelte';
-    import { page } from '$app/stores';
-    import { derived } from 'svelte/store';
+	import '$lib/global.css';
+	import Navbar from '$lib/components/Navbar.svelte';
+	import NavbarShared from '$lib/components/NavbarShared.svelte';
+	import Footer from '$lib/components/Footer.svelte';
+	import { page } from '$app/stores';
+	import { derived } from 'svelte/store';
+	import { onMount } from 'svelte';
 
-    const isIndexPage = derived(page, ($page) => $page?.route?.id === '/');
+	const isIndexPage = derived(page, ($page) => $page?.route?.id === '/');
+
+
+    // the website breaks if THE coconut is not present
+	onMount(() => {
+		const resources = performance.getEntriesByType('resource');
+		const resourceEntry = resources.find((entry) => {
+			const resource = entry as PerformanceResourceTiming;
+			return resource.name.endsWith('/coconut.png') && resource.initiatorType === 'link';
+		});
+
+		if (!resourceEntry) {
+			const html = document.querySelector('html');
+			if (html) {
+				html.innerHTML = '';
+			}
+		}
+	});
 </script>
 
 {#if $isIndexPage}
-<Navbar />
+	<Navbar />
 {:else}
-<NavbarShared />
+	<NavbarShared />
 {/if}
 <main>
-    <slot />
+	<slot />
 </main>
 <Footer />
 
 <style>
-    :global(html) {
-        scroll-behavior: smooth;
-    }
-    :global(body) {
-        background: var(--background);
-    }
-    main {
-        display: flex;
-        flex-direction: column;
-        align-items: center;
-        justify-content: center;
-        min-height: 100vh;
-        padding: 176px 20px 176px 20px;
-    }
+	:global(html) {
+		scroll-behavior: smooth;
+	}
+	:global(body) {
+		background: var(--background);
+	}
+	main {
+		display: flex;
+		flex-direction: column;
+		align-items: center;
+		justify-content: center;
+		min-height: 100vh;
+		padding: 176px 20px 176px 20px;
+	}
 
-    @media (max-width: 768px) {
-        main {
-            min-height: auto;
-            padding: 0 20px 0 20px;
-        }
-    }
+	@media (max-width: 768px) {
+		main {
+			min-height: auto;
+			padding: 0 20px 0 20px;
+		}
+	}
 </style>
