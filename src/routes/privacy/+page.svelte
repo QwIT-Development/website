ec7376aedcf034253769b237f15760c32b8d1158
anchietae <contact@anchietae.cc>
2025-08-07 13:46:26 +0200

    yeah, this will work

--- src/routes/privacy/+page.svelte
+++ src/routes/privacy/+page.svelte
@@ -1,59 +1,78 @@
-<main>
+<script lang="ts">
+	import { onMount } from 'svelte';
+	import HuPolicy from './hu.svelte';
+
+	let isHungarian = false;
+
+	onMount(() => {
+		const lang = navigator.language;
+		if (lang?.toLowerCase().includes('hu')) {
+			isHungarian = true;
+		}
+	});
+</script>
+
+<main style={isHungarian ? 'display: none;' : ''}>
 	<div class="title">
-		<h2 class="font_web_h2">Adatkezelési Tájékoztató</h2>
+		<h2 class="font_web_h2">Privacy Policy</h2>
 	</div>
 	<ul>
 		<li class="font_body_16px_regular">
-			A <b>Firka</b> (továbbiakban: "alkalmazás") egy mobilos, asztali és webes kliensalkalmazás, segítségével
-			az e-Kréta rendszeréből letöltheted és felhasználóbarát módon megjelenítheted az adataidat. Tanulmányi
-			adataid csak közvetlenül az alkalmazás és a Kréta-szerverek között közlekednek, titkosított kapcsolaton
-			keresztül.
+			<b>Firka</b> (hereinafter: "application") is a mobile, desktop, and web client application that
+			allows you to download and display your data from the e-Kréta system in a user-friendly way. Your
+			academic data is transmitted only directly between the application and the Kréta servers over an
+			encrypted connection.
 		</li>
 		<li class="font_body_16px_regular">
-			A Firka fejlesztői és/vagy üzemeltetői, valamint az alkalmazás a tanulmányi és személyes
-			adataidat semmilyen célból és semmilyen körülmények között nem másolják, nem tárolják és
-			harmadik félnek nem továbbítják. Ezeket így az Educational Development Informatikai Zrt.
-			kezeli, az Ő adatkezeléssel kapcsolatos tájékoztatójukat itt találod:
+			The developers and/or operators of Firka, as well as the application itself, do not copy,
+			store, or forward your academic and personal data to third parties for any purpose or under
+			any circumstances. This data is managed by Educational Development Informatikai Zrt. You can
+			find their privacy policy here:
 			<a href="https://tudasbazis.ekreta.hu/pages/viewpage.action?pageId=4065038"
-				>e-Kréta Adatkezelési Tájékoztató</a
+				>e-Kréta Privacy Policy</a
 			>
 			<ul>
 				<li class="font_body_16px_regular">
-					Azok törlésével vagy módosítával kapcsolatban keresd az osztályfőnöködet vagy az iskolád
-					rendszergazdáját.
+					To delete or modify this data, contact your homeroom teacher or your school's system
+					administrator.
 				</li>
 			</ul>
 		</li>
 		<li class="font_body_16px_regular">
-			Az alkalmazás névtelen használati statisztikákat gyűjt, ezek alapján tudjuk meghatározni a
-			felhasználók és a telepítések számát, valamint az eszközük platformját. Ezt a beállításokban
-			kikapcsolhatod. Kérünk, hogy ha csak teheted, hagyd ezt a funkciót bekapcsolva, hogy pontosabb
-			információnk legyen a felhasználóink platform-megoszlásáról.
+			The application collects anonymous usage statistics to determine the number of users and
+			installations, as well as their device platforms. You can disable this in the settings. We ask
+			that you keep this feature enabled if possible, so that we have more accurate information
+			about the platform distribution of our users.
 		</li>
 		<li class="font_body_16px_regular">
-			Amikor az alkalmazás hibába ütközik, lehetőség van hibajelentés küldésére. Ez személyes-
-			és/vagy tanulmányi adatokat nem tartalmaz, viszont részletes információval szolgál a hibáról,
-			annak okáról és eszközödről. A küldés előtt megjelenő képernyőn a te felelősséged átnézni a
-			továbbításra kerülő adatsort. A hibajelentéseket egy privát Discord szobában tároljuk, ezekhez
-			csak az app fejlesztői férnek hozzá.
+			When the application encounters an error, you have the option to send an error report. This
+			report does not contain personal and/or academic data, but it does provide detailed
+			information about the error, its cause, and your device. It is your responsibility to review
+			the data to be sent on the screen that appears before sending. Error reports are stored in a
+			private Discord room, accessible only to the app's developers.
 		</li>
 		<li class="font_body_16px_regular">
-			Az alkalmazás (az alábbi platformokon: Android, Linux, Windows) minden egyes indításakor a
-			Firka API, valamint a Github API segítségével ellenőrzi, hogy elérhető-e új verzió, és kérésre
-			innen letölti és telepíti a frissítést.
+			Each time the application is launched (on the following platforms: Android, Linux, Windows),
+			it checks for a new version using the Firka API and the Github API, and upon request,
+			downloads and installs the update from there.
 		</li>
 		<li class="font_body_16px_regular">
-			Amennyiben az adataiddal kapcsolatban bármilyen kérdésed van (megtekintés, törlés, módosítás,
-			adathordozás), keress minket a <a href="mailto:social@firka.app">social@firka.app</a> e-mail címen,
-			vagy Discord szerverünkön!
+			If you have any questions regarding your data (viewing, deleting, modifying, data
+			portability), contact us at the email address <a href="mailto:social@firka.app"
+				>social@firka.app</a
+			>, or on our Discord server!
 		</li>
 		<li class="font_body_16px_regular">
-			A kliensalkalmazás bármely eszközön és platformon történő használatával tudomásul vetted és
-			elfogadod a jelen adatkezelési tájékoztatót. A Firka csapata fenntartja a jogot a tájékoztató
-			módosítására és a módosításokról nem köteles értesíteni a felhasználóit!
+			By using the client application on any device and platform, you acknowledge and accept this
+			privacy policy. The Firka team reserves the right to modify this policy and is not obligated
+			to notify its users of any changes!
 		</li>
 	</ul>
 </main>
+
+{#if isHungarian}
+	<HuPolicy />
+{/if}
 
 <style>
 	main {
