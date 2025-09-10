f8d8bc6c7d9cf03412c86c11761e6c9899976ba7
Zsolt <dmzsolt12@gmail.com>
2025-09-10 17:16:50 +0200

    Kedves Balázs!

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,19 +1,10 @@
 <script lang="ts">
 	import '$lib/global.css';
 	import Navbar from '$lib/components/Navbar.svelte';
-	import NavbarShared from '$lib/components/NavbarShared.svelte';
 	import Footer from '$lib/components/Footer.svelte';
-	import { page } from '$app/stores';
-	import { derived } from 'svelte/store';
-
-	const isIndexPage = derived(page, ($page) => $page?.route?.id === '/');
 </script>
 
-{#if $isIndexPage}
-	<Navbar />
-{:else}
-	<NavbarShared />
-{/if}
+<Navbar />
 <main>
 	<slot />
 </main>
